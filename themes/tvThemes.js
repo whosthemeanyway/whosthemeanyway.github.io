9d71048cd8af05d065bfef17e4daecfd15212b00
Jake <jacobkmiller@icloud.com>
2022-01-15 18:42:08 -0800

    added music

---
+++ themes/tvThemes.js
@@ -0,0 +1,401 @@
+export const tvThemes = [{
+  id: '1YUVAMkwFHv1IdnNfR0AYx',
+  name: 'Doctor Who (Original Theme) [From "Doctor Who"]'
+},
+{
+  id: '4th0mPYPuZUs564oCu5I1U',
+  name: 'Materia Primoris: The X-Files Theme (Main Title)'
+},
+{
+  id: '51pqlgzFBtqcRmb1CKdc56',
+  name: 'House Of Cards Main Title Theme'
+},
+{
+  id: '3BTaoCnALCV0ncJvIOB9t6',
+  name: 'Suite (From "The West Wing")'
+},
+{
+  id: '44LIBBXjPHbvSf9WQZcJ1O',
+  name: 'The Newsroom Main Theme'
+},
+{
+  id: '67Hna13dNDkZvBpTXRIaOJ',
+  name: 'Teardrop'
+},
+{
+  id: '1yXmxP5gB3x7ut0G3p6R9R',
+  name: `Cosy in the Rocket (From "Grey's Anatomy")`
+},
+{
+  id: '0IpOLDDlfk0I902oJMIBdK',
+  name: 'Law & Order'
+},
+{
+  id: '2BndJYJQ17UcEeUFJP5JmY',
+  name: 'Paradise Circus'
+},
+{
+  id: '45DI4CChW96GTM25oDMKqS',
+  name: 'Way Down In The Hole'
+},
+{
+  id: '62x8ljKy1vKp1o7PG9yIW3',
+  name: 'Dexter: Main Title'
+},
+{
+  id: '7ur68pH4FT6DUhVvm16a3B',
+  name: 'The Simpsons Theme - Orchestral Version'
+},
+{
+  id: '6xCeJ6IUGBLQX5OAfX12Fu',
+  name: "The Bob's Burgers Theme Song"
+},
+{
+  id: '2fYwXajnfsfp4APMSop4Uc',
+  name: "BoJack's Theme"
+},
+{
+  id: '6T0aEcPCC6fiAyZDznr9e0',
+  name: 'King of the Hill Theme'
+},
+{
+  id: '2UD1zyPjnvfMEBpTMXWnCq',
+  name: 'Rick and Morty Main Theme - Cover Version'
+},
+{
+  id: '4t20umh0eSuYstasrSl2HV',
+  name: 'Main Title'
+},
+{
+  id: '0ldqqFB8HUQCxuAhGIwlQG',
+  name: 'Theme'
+},
+{
+  id: '0rSJtqCSAkhKORRpJlk7pL',
+  name: 'The Punisher Main Title'
+},
+{
+  id: '7s91gb6uV5PPDq6muq6z9g',
+  name: 'Big Bang Theory Theme'
+},
+{
+  id: '5NUCLgfK7d3zJeOJN0hZIv',
+  name: 'The Office Theme'
+},
+{
+  id: '2dJuJ1e14nf47qtG5CYrr4',
+  name: 'Parks And Recreation - Main Theme'
+},
+{
+  id: '4HUUzLq4GpjaBETWz6UZYj',
+  name: 'Brooklyn Nine-Nine'
+},
+{
+  id: '6RK0oFMDxPsxcm6tgxy6kg',
+  name: 'Temptation Sensation (Main Title)'
+},
+{
+  id: '5buHpJ3TPW4kKpH8V4MJIr',
+  name: 'The League Theme'
+},
+{
+  id: '0pkS386R0P2Jy2NkAcDnx5',
+  name: 'Stretch Your Face'
+},
+{
+  id: '0UREO3QWbXJW3gOUXpK1am',
+  name: 'The Fresh Prince of Bel-Air'
+},
+{
+  id: '7H0lD5db9HGhFljO68B2Ht',
+  name: "That 70's Song - Live"
+},
+{
+  id: '497Fkp3gRiGrRMoqBTDudr',
+  name: 'California - Tchad Blake Mix'
+},
+{
+  id: '4lIFhMHEy8HmUWB3WVthcB',
+  name: 'Unbreakable Kimmy Schmidt Theme Song'
+},
+{
+  id: '4V0x90QcMh4ZxwHzEWOdtK',
+  name: 'Feel It All Around'
+},
+{
+  id: '3YikZfl52FfOdK0vg0BIB5',
+  name: 'Arrested Development - Credits Theme'
+},
+{
+  id: '1xOufOKaxfBlxG6l7ytHZ0',
+  name: 'Seinfeld'
+},
+{
+  id: '5foxQ24C0x7W0B2OD46AJg',
+  name: 'Frolic (Curb Your Enthusiasm Theme)'
+},
+{
+  id: '747LUtpzObW9yhg3f0TmYC',
+  name: 'Veep - Cover Version'
+},
+{
+  id: '12hjR86EfWjwGQkZJRrKKp',
+  name: 'Californication -Theme from the Showtime Original Series'
+},
+{
+  id: '2QkarEslO6sZFeHxUK7HdZ',
+  name: 'Homeland - Theme - Cover Version'
+},
+{
+  id: '13MxuiI1zCvxFYKoP0Bpye',
+  name: 'Monk Theme - Extended Version'
+},
+{
+  id: '2wqmfNYxkxfw2KHRqkxTA1',
+  name: 'Sex And The City Theme'
+},
+{
+  id: '7MRm5DP3WMB6sAK5UqeK4S',
+  name: 'The Brady Bunch'
+},
+{
+  id: '7M2t3mrwREenfKCvpRCed7',
+  name: 'Main Theme: The Addams Family - Instrumental Version'
+},
+{
+  id: '0r8DL3CcKkbBrDuIre8DmY',
+  name: 'Andy Griffith Show, The - Theme from the TV Series'
+},
+{
+  id: '1KSrq0kn9b7smt2aaEHxOZ',
+  name: 'Leave It to Beaver'
+},
+{
+  id: '48KprKXSaqDzGCO8SAnv8s',
+  name: 'Main Title (From Game of Thrones: Season 6)'
+},
+{
+  id: '1zRxId8yeo2i2d1A1U289h',
+  name: 'Woke up This Morning (The Sopranos Mix)'
+},
+{
+  id: '2hsLpiKNkWpd4e9QuVdhar',
+  name: 'Breaking Bad Main Title Theme (Extended)'
+},
+{
+  id: '7bfsr4blCxi101cuqR1JKo',
+  name: 'Better Call Saul Main Title Theme (Extended)'
+},
+{
+  id: '0QmIPFQ3h0yVPgHsfqpy05',
+  name: '1.0_8-whatsyourask.m4p'
+},
+{
+  id: '1qZrZpFbGdJBwBf9QQflHo',
+  name: 'Straight Up And Down'
+},
+{
+  id: '6Dc2tCivms1s2X9myWe4ni',
+  name: 'Tuyo (Narcos Theme) - A Netflix Original Series Soundtrack'
+},
+{
+  id: '45hyeKhkraLuIwj2qKEuU1',
+  name: 'Long Hard Times to Come'
+},
+{
+  id: '3LDkLpuxQlEuEiZmkxpr8S',
+  name: 'Far From Any Road'
+},
+{
+  id: '5u1YbXeoLpDuunADJIxQpE',
+  name: 'Bemidji, MN (Fargo Series Main Theme)'
+},
+{
+  id: '1LcNtS9B22LgIHxd9gTGcV',
+  name: 'Main Titles from the HBO Miniseries Band of Brothers - Instrumental'
+},
+{
+  id: '7COqi3bsIF5j3uqODbWGoM',
+  name: 'Inspector Gadget'
+},
+{
+  id: '3OIHgTyQdiAGMmpjQaNxp3',
+  name: 'Pokémon Theme'
+},
+{
+  id: '0qeKzbUsW0V4ZWRJrHNiD3',
+  name: 'Bad Boys (Theme from COPS)'
+},
+{
+  id: '2LUVo82IKIpCuBNSaqF0G8',
+  name: 'Corona'
+},
+{
+  id: '1AYi8wbamIha0MYzNie76B',
+  name: 'Get Crazy'
+},
+{
+  id: '5Hf3rVitK172yr1pVyKy7f',
+  name: 'American Idol Theme'
+},
+{
+  id: '3p1JoOEhVkEnTaa4JzTMSk',
+  name: 'For the Love of Money'
+},
+{
+  id: '006UW0dFINfrc8MQsLXsHi',
+  name: 'Modern Family - Cover Version'
+},
+{
+  id: '0WKgf2szFWxT1k0orm8PAc',
+  name: 'Tossed Salad and Scrambled Eggs (From "Frasier")'
+},
+{
+  id: '5SlKhhXtzd3Cj74HYmHZNz',
+  name: "I'll Be There for You (Theme from Friends) - Single Version"
+},
+{
+  id: '3ffJKEoVfbiSlDHztViu6O',
+  name: 'Superman'
+},
+{
+  id: '6z4CTIToU7a03Rq07F9FF4',
+  name: 'Superhero'
+},
+{
+  id: '7cz70nyRXlCJOE85whEkgU',
+  name: 'Flagpole Sitta'
+},
+{
+  id: '6zBDHSuanN16ulOFKYSOhC',
+  name: "Monty Python's Flying Circus - The Liberty Bell"
+},
+{
+  id: '0gthY07uquYTZXfg1Kc5EX',
+  name: 'SpongeBob SquarePants Theme Song'
+},
+{
+  id: '1dBy6Cfzh0UkUoLWYhqGHN',
+  name: 'Rugrats Theme (From "Rugrats") - Instrumental'
+},
+{
+  id: '1QPRmX2e3EZWskuOe5QqxM',
+  name: 'Pink Panther Theme'
+},
+{
+  id: '08IlKB78DmwX74X5vAaunj',
+  name: 'Theme from South Park (In the Style of Primus) [South Park] [Karaoke Version]'
+},
+{
+  id: '72jm8yTVBe8LKbvr3EU8gR',
+  name: 'Family Guy Main Title - From "Family Guy"'
+},
+{
+  id: '6BCV23kVtJXuXah3R8UhKg',
+  name: 'Theme From I Dream of Jeannie'
+},
+{
+  id: '0S2LY6DueQH9tljXnAsVwF',
+  name: 'Bewitched'
+},
+{
+  id: '6jKYMsHYg4rqY2H3C0zx6c',
+  name: 'Full House'
+},
+{
+  id: '46Wjz7E4p56QujysV3l2IN',
+  name: 'Home Improvement'
+},
+{
+  id: '5hwrGt6JjNRaec35dbPz5V',
+  name: 'American Horror Story Theme (Tv Version)'
+},
+{
+  id: '772wRzI6VkJmYYX6UDsN7k',
+  name: 'The Haunting of Hill House (Main Titles)'
+},
+{
+  id: '1JcCsETRv85wmHrI26EuTS',
+  name: 'Stranger Things Theme'
+},
+{
+  id: '6T02cE4ODmo9nHsO9Is3Y0',
+  name: 'The Game is On'
+},
+{
+  id: '0qHeP8zt2WWef7EWCs1ECj',
+  name: 'Red Right Hand - 2011 Remastered Version'
+},
+{
+  id: '6nOmoLgN8P8ZojNJMnPOnf',
+  name: 'Downton Abbey - The Suite - From “Downton Abbey” Soundtrack'
+},
+{
+  id: '1puqmKaUU7s2v00XkZeFfG',
+  name: "Charlie's Angels"
+},
+{
+  id: '6XhLvHFFGw89NbGfpINRyn',
+  name: 'Reading Rainbow Theme Song'
+},
+{
+  id: '4PSgx8uleKin89LFvvSXf9',
+  name: 'Dora The Explorer Party Mix (including "Dora The Explorer Theme" & "Travel Song")'
+},
+{
+  id: '4J4wYsDnJxldGxAiFpIIzT',
+  name: "Blue's Clues Theme Song"
+},
+{
+  id: '4Zp7iCDJU2BfD7SOLuSa6X',
+  name: 'Barney Theme Song'
+},
+{
+  id: '1IlaO6ioyPvuAziyK7oW9g',
+  name: 'Lois and Clark - The New Adventures Of Superman'
+},
+{
+  id: '0dRvjBVCStIlBxOqbhAQ3q',
+  name: 'Little Boxes'
+},
+{
+  id: '1x8D2YdP5pQrXX2cZmrWr8',
+  name: 'The Luck You Got'
+},
+{
+  id: '3dua9ntHlxONJryjfLfPwA',
+  name: 'Change for the World'
+},
+{
+  id: '1wvlEOabKqOsAuTC8ES15F',
+  name: 'Theme (From "Buffy the Vampire Slayer")'
+},
+{
+  id: '7hpFYWL3cw5m4y70cce7Zb',
+  name: 'We Used To Be Friends'
+},
+{
+  id: '4mC63vvLta2BwsF32cCpS8',
+  name: 'Jeopardy - Main Theme (Single)'
+},
+{
+  id: '1sQjj3PHP2UHKxfe2Fqhyo',
+  name: 'Family Feud'
+},
+{
+  id: '20alNh0CJnzfwMD3DklbwP',
+  name: 'The Price Is Right - Game Show Theme'
+},
+{
+  id: '3Dbx1Beq28MDIJTMv8sU8C',
+  name: 'The Walking Dead MT'
+},
+{
+  id: '31Rb9klHausEwGGOBhQtnm',
+  name: 'Orphan Black Theme'
+},
+{
+  id: '7z6DpzZo3l5pojIx0GhdWa',
+  name: 'Heroes (Main Theme)'
+}
+]
